9bd7ada842f60a3741a73ad5309fcf8f1b60a78e
Alex Farrell <ralexfarrell@outlook.com>
2025-10-28 12:56:55 -0400

    Implemented calculate measured field strength behavior (passing)

--- src/microstrain_mag_cal.cpp
+++ src/microstrain_mag_cal.cpp
@@ -4,6 +4,16 @@ namespace MicrostrainMagCal
 {
     double calculate_measured_field_strength(const Eigen::MatrixX3d &points)
     {
+        if (points.rows() == 0)
+        {
+            return 0.0;
+        }
 
+        // TODO: Move to separate internal function
+        // -------------------------------
+        const Eigen::RowVector3d field_offset = points.colwise().mean();
+        // -------------------------------
+
+        return (points.rowwise() - field_offset).rowwise().norm().mean();
     }
 }
