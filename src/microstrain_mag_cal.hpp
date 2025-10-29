b06a17d3c9f08afa29d6b0f85868441e040daa42
Alex Farrell <ralexfarrell@outlook.com>
2025-10-29 13:52:26 -0400

    Implemented calculate fit RMSE behavior (passing)

--- src/microstrain_mag_cal.hpp
+++ src/microstrain_mag_cal.hpp
@@ -8,4 +8,9 @@ namespace MicrostrainMagCal
     double calculate_measured_field_strength(const Eigen::MatrixX3d &points);
     // TODO: Document using Geography convention over Physics convention.
     double calculate_spatial_coverage(const Eigen::MatrixX3d &points);
+    double calculateFitRMSE(
+        const Eigen::MatrixX3d &points,
+        const Eigen::Matrix<double, 3, 3> &soft_iron_matrix,
+        const Eigen::Vector3d &hard_iron_offset,
+        double field_strength);
 }
