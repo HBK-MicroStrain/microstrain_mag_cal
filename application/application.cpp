616c3dc780fb1356a241eea50813109a51563584
Alex Farrell <ralexfarrell@outlook.com>
2025-10-30 14:30:32 -0400

    Application: Moved file mapping logic into dedicated function

--- application/application.cpp
+++ application/application.cpp
@@ -3,18 +3,32 @@
 #include "mio/mmap.hpp"
 
 
-int main()
+/// Creates a read-only memory-mapped view of the given file.
+mio::mmap_source map_file(const std::string &file_path, std::error_code &error)
 {
-    std::error_code error;
-    const mio::mmap_source map_source = mio::make_mmap_source("test_file_for_offline_mag.bin", error);
+    mio::mmap_source read_only_mapping = mio::make_mmap_source(file_path, error);
 
     if (error)
     {
         std::cerr << "Error opening file: " << error.message() << std::endl;
+    }
+
+    return read_only_mapping;
+}
+
+
+int main()
+{
+    std::error_code error;
+    const mio::mmap_source file_mapping = map_file("test_file_for_offline_mag.bin", error);
+
+    if (error)
+    {
         return 1;
     }
 
-    const char *data = map_source.data();
+    const char *data = file_mapping.data();
+    const size_t data_size = file_mapping.size();
 
     return 0;
 }
