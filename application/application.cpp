e272423b9a867ae08e0234e8a804842a86402e99
Alex Farrell <ralexfarrell@outlook.com>
2025-10-30 13:08:44 -0400

    Created memory map for test file in commandline application

--- application/application.cpp
+++ application/application.cpp
@@ -1,7 +1,20 @@
+#include <iostream>
 
-const char *TEST_FILE_PATH = "test_file_for_offline_mag.bin";
+#include "mio/mmap.hpp"
+
 
 int main()
 {
+    std::error_code error;
+    const mio::mmap_source map_source = mio::make_mmap_source("test_file_for_offline_mag.bin", error);
+
+    if (error)
+    {
+        std::cerr << "Error opening file: " << error.message() << std::endl;
+        return 1;
+    }
+
+    const char *data = map_source.data();
+
     return 0;
 }
