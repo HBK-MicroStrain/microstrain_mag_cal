ebffeb6ed923314c13844f5f039554964c8d070f
Alex Farrell <ralexfarrell@outlook.com>
2025-10-31 13:44:21 -0400

    Removed utility function for file mapping

--- application/application.cpp
+++ application/application.cpp
@@ -7,19 +7,6 @@
 using ScaledMag = mip::data_sensor::ScaledMag;
 
 
-/// Creates a read-only memory-mapped view of the given file.
-mio::mmap_source map_file(const std::string &file_path, std::error_code &error)
-{
-    mio::mmap_source file_mapping = mio::make_mmap_source(file_path, error);
-
-    if (error)
-    {
-        std::cerr << "Error opening file: " << error.message() << std::endl;
-    }
-
-    return file_mapping;
-}
-
 // TODO: Implement
 void addPointToMatrix(void *matrix, const mip::FieldView &field_view, mip::Timestamp timestamp)
 {
@@ -29,17 +16,20 @@ void addPointToMatrix(void *matrix, const mip::FieldView &field_view, mip::Times
 
 int main()
 {
+    // Map a read-only view of the given file.
     std::error_code error;
-    const mio::mmap_source file_mapping = map_file("test_file_for_offline_mag.bin", error);
+    const mio::mmap_source file_mapping = mio::make_mmap_source("test_file_for_offline_mag.bin", error);
 
     if (error)
     {
+        std::cerr << "Error opening file: " << error.message() << std::endl;
         return 1;
     }
 
     const uint8_t *data = reinterpret_cast<const uint8_t *>(file_mapping.data());
     const size_t data_size = file_mapping.size();
 
+    // TODO: Set up two-pass approach to pre-allocate matrix
     // Extract a matrix of points from the raw binary data.
     // We aren't working with a device, so the timeouts and timestamp aren't needed.
     mip::Interface data_parser(0, 0);
