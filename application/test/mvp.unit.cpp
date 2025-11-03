a6deb8d631bf8d6736ed2adb9db056d1407de775
Alex Farrell <ralexfarrell@outlook.com>
2025-11-03 15:14:55 -0500

    Added noise method to binary data factory and setup test for extracting point matrix from raw data (failing)

--- application/test/mvp.unit.cpp
+++ application/test/mvp.unit.cpp
@@ -2,6 +2,7 @@
 #include <microstrain_test/microstrain_test.hpp>
 #include <mip/mip_packet.h>
 
+
 class BinaryDataBuilder
 {
 public:
@@ -20,27 +21,67 @@ public:
         mip::C::mip_packet_finalize(&packet);
 
         const uint8_t *packet_data = mip::C::mip_packet_buffer(&packet);
-        const uint8_t packet_length = mip::C::mip_packet_total_length(&packet);
+        const uint_least16_t packet_length = mip::C::mip_packet_total_length(&packet);
 
         m_data.insert(m_data.end(), packet_data, packet_data + packet_length);
 
         return *this;
     }
 
+    BinaryDataBuilder& addNoise(const std::initializer_list<uint8_t> noise)
+    {
+        m_data.insert(m_data.end(), noise.begin(), noise.end());
+
+        return *this;
+    }
+
+    const uint8_t* data() const
+    {
+        return m_data.data();
+    }
+
+    size_t data_size() const
+    {
+        return m_data.size();
+    }
+
 private:
     std::vector<uint8_t> m_data;
 };
 
-// TODO: Update with proper output
-// Data points taken from a real InertialConnect binary recording. All expected values for tests are
-// taken from InertialConnect based off this input data.
-static constexpr std::array<double, 20 * 3> raw_points = {
-    /*  1 */ 0.177713 ,  0.0808103382, -0.213378355 ,
+static constexpr std::array<double, 3 * 3> raw_points = {
+      1.123456789 ,  -2.123456789,   3.123456789,
+     -4.123456789 ,   5.123456789,  -6.123456789,
+      7.123456789 ,  -8.123456789,   9.123456789,
 };
-static const Eigen::Matrix<double, 20, 3, Eigen::RowMajor> CHECK_POINTS(raw_points.data());
+static const Eigen::Matrix<double, 3, 3, Eigen::RowMajor> CHECK_POINTS(raw_points.data());
 
 
 MICROSTRAIN_TEST_CASE("MVP", "Extracting_a_points_matrix_contains_all_points_from_the_raw_data")
 {
-    // TODO: Implement
+    BinaryDataBuilder builder = BinaryDataBuilder()
+        .addNoise({0xFF, 0xAB, 0xCD, 0xEF})
+        .addMagCalPointVector(1.123456789f,  -2.123456789f,   3.123456789f)
+        .addNoise({0xFF, 0xAB, 0xCD, 0xEF})
+        .addNoise({0xFF, 0xAB, 0xCD, 0xEF})
+        .addMagCalPointVector(-4.123456789f,   5.123456789f,  -6.123456789f)
+        .addNoise({0xFF, 0xAB, 0xCD, 0xEF})
+        .addNoise({0xFF, 0xAB, 0xCD, 0xEF})
+        .addNoise({0xFF, 0xAB, 0xCD, 0xEF})
+        .addMagCalPointVector(7.123456789f,  -8.123456789f,   9.123456789f)
+        .addNoise({0xFF, 0xAB, 0xCD, 0xEF});
+    const uint8_t *data = builder.data();
+    const size_t data_size = builder.data_size();
+
+    Eigen::MatrixX3d result = Core::extractPointMatrixFromRawData(data, data_size);
+
+    CHECK(result(0, 0) == doctest::Approx(1.123456789).epsilon(0.001));
+    CHECK(result(0, 1) == doctest::Approx(-2.123456789).epsilon(0.001));
+    CHECK(result(0, 2) == doctest::Approx(3.123456789).epsilon(0.001));
+    CHECK(result(1, 0) == doctest::Approx(-4.123456789).epsilon(0.001));
+    CHECK(result(1, 1) == doctest::Approx(5.123456789).epsilon(0.001));
+    CHECK(result(1, 2) == doctest::Approx(-6.123456789).epsilon(0.001));
+    CHECK(result(2, 0) == doctest::Approx(7.123456789).epsilon(0.001));
+    CHECK(result(2, 1) == doctest::Approx(-8.123456789).epsilon(0.001));
+    CHECK(result(2, 2) == doctest::Approx(9.123456789).epsilon(0.001));
 }
