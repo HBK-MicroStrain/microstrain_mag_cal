c6aeac92f4b2ae2af5a5bc13e66fa89a77c3ff86
Alex Farrell <ralexfarrell@outlook.com>
2025-11-03 11:25:18 -0500

    Setup core library for application backend code

--- application/src/application.cpp
+++ application/src/application.cpp
@@ -3,12 +3,9 @@
 #include <Eigen/Dense>
 #include <mio/mmap.hpp>
 
+#include <core.hpp>
 #include <microstrain/platform.h>
 #include <mip/mip_parser.hpp>
-#include <mip/definitions/data_sensor.hpp>
-
-
-using ScaledMag = mip::data_sensor::ScaledMag;
 
 
 bool countPointsInRawData(void *num_points, const mip::PacketView *packet_view, mip::Timestamp timestamp)
@@ -37,10 +34,22 @@ bool extractPointsToMatrix(Eigen::MatrixX3d &points_matrix, const mip::PacketVie
 {
     if (packet_view.descriptorSet() == ScaledMag::DESCRIPTOR_SET)
     {
-        for (const mip::FieldView &field : packet_view)
+        for (const mip::FieldView &field_view : packet_view)
         {
-            if (field.fieldDescriptor() == ScaledMag::FIELD_DESCRIPTOR)
+            if (field_view.fieldDescriptor() == ScaledMag::FIELD_DESCRIPTOR)
             {
+                float extracted_point[3];
+                microstrain::serialization::big_endian::read(field_view.payload().data(), extracted_point[0]);
+                microstrain::serialization::big_endian::read(field_view.payload().data(), extracted_point[0]);
+                microstrain::serialization::big_endian::read(field_view.payload().data(), extracted_point[0]);
+
+                // TODO: Change to create matrix row vector and add to matrix
+                // TODO: Move this to inside loop over discovered points
+                double converted_point[3];
+                for (uint8_t i = 0; i < 3; ++i)
+                {
+                    converted_point[i] = static_cast<double>(extracted_point[i]);
+                }
             }
         }
     }
@@ -66,12 +75,21 @@ int main()
 
     // Pass 1: Pre-allocate matrix
     // We aren't working with a device, so the timeouts and timestamp aren't needed.
+    // TODO: Setup point estimate to pre-allocate data pointer vector
+    // TODO: Change count callback to extract the data pointers to the vector
+    // TODO: Use vector size to pre-allocate matrix
     size_t num_points = 0;
     mip::Parser parser(&countPointsInRawData, &num_points, 0);
     parser.parse(data, data_size, 0);
     Eigen::MatrixX3d points_matrix(num_points, 3);
 
+    // TODO: Switch to loop over vector of discovered points instead
+    // TODO: Set to extract data from vector of points into matrix
     // Pass 2: Extract points from the binary data into the matrix.
+    for (size_t i = 0; i < num_points; ++i)
+    {
+       // Get next field
+    }
     parser.setCallback<Eigen::MatrixX3d, &extractPointsToMatrix>(points_matrix);
 
     // TODO: For testing, remove after
---
+++ application/src/core.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <mip/definitions/data_sensor.hpp>
+
+using ScaledMag = mip::data_sensor::ScaledMag;
+
+
