556a28c4bd30746493c420c2f600f5cf48856064
Alex Farrell <ralexfarrell@outlook.com>
2025-11-03 11:45:20 -0500

    Added extract point matrix interface in core and moved all implementation logic into core

--- application/src/application.cpp
+++ application/src/application.cpp
@@ -1,62 +1,8 @@
 #include <iostream>
 
-#include <Eigen/Dense>
 #include <mio/mmap.hpp>
 
 #include <core.hpp>
-#include <microstrain/platform.h>
-#include <mip/mip_parser.hpp>
-
-
-bool countPointsInRawData(void *num_points, const mip::PacketView *packet_view, mip::Timestamp timestamp)
-{
-    if (packet_view == nullptr)
-    {
-        return false;
-    }
-
-    if (packet_view->descriptorSet() == ScaledMag::DESCRIPTOR_SET)
-    {
-        for (const mip::FieldView &field : *packet_view)
-        {
-            if (field.fieldDescriptor() == ScaledMag::FIELD_DESCRIPTOR)
-            {
-                size_t *count = static_cast<size_t *>(num_points);
-                ++(*count);
-            }
-        }
-    }
-
-    return true;
-}
-
-bool extractPointsToMatrix(Eigen::MatrixX3d &points_matrix, const mip::PacketView &packet_view, mip::Timestamp timestamp)
-{
-    if (packet_view.descriptorSet() == ScaledMag::DESCRIPTOR_SET)
-    {
-        for (const mip::FieldView &field_view : packet_view)
-        {
-            if (field_view.fieldDescriptor() == ScaledMag::FIELD_DESCRIPTOR)
-            {
-                float extracted_point[3];
-                microstrain::serialization::big_endian::read(field_view.payload().data(), extracted_point[0]);
-                microstrain::serialization::big_endian::read(field_view.payload().data(), extracted_point[0]);
-                microstrain::serialization::big_endian::read(field_view.payload().data(), extracted_point[0]);
-
-                // TODO: Change to create matrix row vector and add to matrix
-                // TODO: Move this to inside loop over discovered points
-                double converted_point[3];
-                for (uint8_t i = 0; i < 3; ++i)
-                {
-                    converted_point[i] = static_cast<double>(extracted_point[i]);
-                }
-            }
-        }
-    }
-
-    return true;
-}
-
 
 int main()
 {
@@ -73,28 +19,9 @@ int main()
     const uint8_t *data = reinterpret_cast<const uint8_t *>(file_mapping.data());
     const size_t data_size = file_mapping.size();
 
-    // Pass 1: Pre-allocate matrix
-    // We aren't working with a device, so the timeouts and timestamp aren't needed.
-    // TODO: Setup point estimate to pre-allocate data pointer vector
-    // TODO: Change count callback to extract the data pointers to the vector
-    // TODO: Use vector size to pre-allocate matrix
-    size_t num_points = 0;
-    mip::Parser parser(&countPointsInRawData, &num_points, 0);
-    parser.parse(data, data_size, 0);
-    Eigen::MatrixX3d points_matrix(num_points, 3);
+    Eigen::MatrixX3d point_matrix = Core::extractPointMatrixFromRawData(data, data_size);
 
-    // TODO: Switch to loop over vector of discovered points instead
-    // TODO: Set to extract data from vector of points into matrix
-    // Pass 2: Extract points from the binary data into the matrix.
-    for (size_t i = 0; i < num_points; ++i)
-    {
-       // Get next field
-    }
-    parser.setCallback<Eigen::MatrixX3d, &extractPointsToMatrix>(points_matrix);
-
-    // TODO: For testing, remove after
-    std::cout << "MATRIX:\n";
-    std::cout << points_matrix.topRows(10) << std::endl;
+    // TODO: Use point_matrix as input to calibration functions
 
     return 0;
 }
--- application/src/core.hpp
+++ application/src/core.hpp
@@ -1,7 +1,14 @@
 #pragma once
 
+#include <Eigen/Dense>
+
+#include <microstrain/platform.h>
 #include <mip/definitions/data_sensor.hpp>
+#include <mip/mip_parser.hpp>
 
 using ScaledMag = mip::data_sensor::ScaledMag;
 
-
+namespace Core
+{
+    Eigen::MatrixX3d extractPointMatrixFromRawData(const uint8_t *data, const size_t data_size);
+}
