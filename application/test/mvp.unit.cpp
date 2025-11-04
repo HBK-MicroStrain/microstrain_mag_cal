2e43828de9a3e875a04dcbd73f047c51bee143b7
Alex Farrell <ralexfarrell@outlook.com>
2025-11-04 16:06:53 -0500

    Changed binary data factory to add mip packets with multiple fields

--- application/test/mvp.unit.cpp
+++ application/test/mvp.unit.cpp
@@ -3,28 +3,24 @@
 #include <microstrain_test/microstrain_test.hpp>
 #include "mip/definitions/data_sensor.hpp"
 
+using ScaledMag = mip::data_sensor::ScaledMag;
 
 // This factory class allows us to create controllable and replicable fake binary data.
 class BinaryDataBuilder
 {
 public:
-    BinaryDataBuilder& addMagCalPointVector(const float x, const float y, const float z)
-    {
-        const mip::PacketBuf packet(mip::data_sensor::ScaledMag{{x, y, z}});
-        m_data.insert(m_data.end(), packet.data().begin(), packet.data().end());
-
-        return *this;
-    }
-
-    // Adds noise to simulate other data in a binary file.
+    // Builds a mip packet and inserts it into the fake binary data.
     //
-    // We don't care what that data is, but it's useful for tests to make sure only the correct
-    // data is extracted, and that all the correct data is extracted. In other words, it removes
-    // potential false negatives that could come from the logic working on just the data to be
-    // extracted, but breaking if there is other data in the input.
-    BinaryDataBuilder& addNoise(const std::initializer_list<uint8_t> noise)
+    // Pass in mip fields to include in the packet. For example:
+    //     ScaledMag{{ 1.0, 2.0, 3.0 }}, ScaledMag{{ 4.0, 5.0, 6.0 }}, ...
+    template<typename... Args>
+    BinaryDataBuilder& addMipPacket(Args&&... args)
     {
-        m_data.insert(m_data.end(), noise.begin(), noise.end());
+        mip::PacketBuf packet(mip::data_sensor::DESCRIPTOR_SET);
+        (packet.addField(std::forward<Args>(args)), ...);
+        packet.finalize();
+
+        m_data.insert(m_data.end(), packet.data().begin(), packet.data().end());
 
         return *this;
     }
@@ -42,16 +38,16 @@ private:
 MICROSTRAIN_TEST_CASE("MVP", "Extracting_a_point_matrix_contains_all_points_from_the_input_binary_data")
 {
     BinaryDataBuilder builder = BinaryDataBuilder()
-        .addNoise({0xFF, 0xAB, 0xCD, 0xEF})
-        .addMagCalPointVector(1.123456789f,  -2.123456789f,   3.123456789f)
-        .addNoise({0xFF, 0xAB, 0xCD, 0xEF})
-        .addNoise({0xFF, 0xAB, 0xCD, 0xEF})
-        .addMagCalPointVector(-4.123456789f,   5.123456789f,  -6.123456789f)
-        .addNoise({0xFF, 0xAB, 0xCD, 0xEF})
-        .addNoise({0xFF, 0xAB, 0xCD, 0xEF})
-        .addNoise({0xFF, 0xAB, 0xCD, 0xEF})
-        .addMagCalPointVector(7.123456789f,  -8.123456789f,   9.123456789f)
-        .addNoise({0xFF, 0xAB, 0xCD, 0xEF});
+        //.addNoise({0xFF, 0xAB, 0xCD, 0xEF})
+        .addMipPacket(ScaledMag{{1.123456789f,  -2.123456789f,   3.123456789f}})
+        //.addNoise({0xFF, 0xAB, 0xCD, 0xEF})
+        //.addNoise({0xFF, 0xAB, 0xCD, 0xEF})
+        .addMipPacket(ScaledMag{{-4.123456789f,   5.123456789f,  -6.123456789f}})
+        //.addNoise({0xFF, 0xAB, 0xCD, 0xEF})
+        //.addNoise({0xFF, 0xAB, 0xCD, 0xEF})
+        //.addNoise({0xFF, 0xAB, 0xCD, 0xEF})
+        .addMipPacket(ScaledMag{{7.123456789f,  -8.123456789f,   9.123456789f}});
+        //.addNoise({0xFF, 0xAB, 0xCD, 0xEF});
     const microstrain::ConstU8ArrayView data_view = builder.data();
 
     Eigen::MatrixX3d result = mag_cal_core::extractPointMatrixFromRawData(data_view);
