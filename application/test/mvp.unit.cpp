1771d74a26c08bcede0a3d9f0f0c23311be630a1
Alex Farrell <ralexfarrell@outlook.com>
2025-11-04 16:27:04 -0500

    Added CHECK_POINTS array

--- application/test/mvp.unit.cpp
+++ application/test/mvp.unit.cpp
@@ -33,20 +33,26 @@ private:
     std::vector<uint8_t> m_data;
 };
 
+std::array CHECK_POINTS {
+     1.123456789f, -2.123456789f,  3.123456789f,
+    -4.123456789f,  5.123456789f, -6.123456789f,
+     7.123456789f, -8.123456789f,  9.123456789f
+};
+
 
 MICROSTRAIN_TEST_CASE("MVP", "Mag_cal_data_can_be_extracted_from_binary_into_a_point_matrix")
 {
     BinaryDataBuilder builder = BinaryDataBuilder()
         .addMipPacket(
-            mip::data_sensor::ScaledMag{{1.123456789f,  -2.123456789f,   3.123456789f}}
+            mip::data_sensor::ScaledMag{{CHECK_POINTS[0], CHECK_POINTS[1], CHECK_POINTS[2]}}
         )
         .addMipPacket(
             mip::data_sensor::ScaledAccel{{0.0f, 0.0f, 0.0f}}, // Noise
-            mip::data_sensor::ScaledMag{{-4.123456789f,   5.123456789f,  -6.123456789f}}
+            mip::data_sensor::ScaledMag{{CHECK_POINTS[3], CHECK_POINTS[4], CHECK_POINTS[5]}}
         )
         .addMipPacket(
             mip::data_sensor::ScaledAccel{{0.0f, 0.0f, 0.0f}}, // Noise
-            mip::data_sensor::ScaledMag{{7.123456789f,  -8.123456789f,   9.123456789f}},
+            mip::data_sensor::ScaledMag{{CHECK_POINTS[6], CHECK_POINTS[7], CHECK_POINTS[8]}},
             mip::data_sensor::ScaledGyro{{0.0f, 0.0f, 0.0f}}    // Noise
         );
     const microstrain::ConstU8ArrayView data_view = builder.data();
@@ -55,13 +61,13 @@ MICROSTRAIN_TEST_CASE("MVP", "Mag_cal_data_can_be_extracted_from_binary_into_a_p
 
     // TODO: Add check for size
 
-    CHECK(result(0, 0) == doctest::Approx(1.123456789).epsilon(0.001));
-    CHECK(result(0, 1) == doctest::Approx(-2.123456789).epsilon(0.001));
-    CHECK(result(0, 2) == doctest::Approx(3.123456789).epsilon(0.001));
-    CHECK(result(1, 0) == doctest::Approx(-4.123456789).epsilon(0.001));
-    CHECK(result(1, 1) == doctest::Approx(5.123456789).epsilon(0.001));
-    CHECK(result(1, 2) == doctest::Approx(-6.123456789).epsilon(0.001));
-    CHECK(result(2, 0) == doctest::Approx(7.123456789).epsilon(0.001));
-    CHECK(result(2, 1) == doctest::Approx(-8.123456789).epsilon(0.001));
-    CHECK(result(2, 2) == doctest::Approx(9.123456789).epsilon(0.001));
+    CHECK(result(0, 0) == doctest::Approx(CHECK_POINTS[0]).epsilon(0.001));
+    CHECK(result(0, 1) == doctest::Approx(CHECK_POINTS[1]).epsilon(0.001));
+    CHECK(result(0, 2) == doctest::Approx(CHECK_POINTS[2]).epsilon(0.001));
+    CHECK(result(1, 0) == doctest::Approx(CHECK_POINTS[3]).epsilon(0.001));
+    CHECK(result(1, 1) == doctest::Approx(CHECK_POINTS[4]).epsilon(0.001));
+    CHECK(result(1, 2) == doctest::Approx(CHECK_POINTS[5]).epsilon(0.001));
+    CHECK(result(2, 0) == doctest::Approx(CHECK_POINTS[6]).epsilon(0.001));
+    CHECK(result(2, 1) == doctest::Approx(CHECK_POINTS[7]).epsilon(0.001));
+    CHECK(result(2, 2) == doctest::Approx(CHECK_POINTS[8]).epsilon(0.001));
 }
