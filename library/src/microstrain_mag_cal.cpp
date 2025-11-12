4e9b64ae891f38e571911b8b47fce92895566b12
Alex Farrell <ralexfarrell@outlook.com>
2025-11-12 11:04:50 -0500

    Moved point manager and voxel grid into separate preprocessing file

--- library/src/microstrain_mag_cal.cpp
+++ library/src/microstrain_mag_cal.cpp
@@ -12,47 +12,6 @@
 namespace microstrain_mag_cal
 {
     // ---------------------------------------------------------------------------------------------
-    // Data Structures
-    // ---------------------------------------------------------------------------------------------
-
-    bool VoxelGrid::isPointInUniqueVoxel(const std::array<float, 3> &point)
-    {
-        const VoxelKey key{
-            static_cast<int>(std::floor(point[0] / m_voxel_size)),
-            static_cast<int>(std::floor(point[1] / m_voxel_size)),
-            static_cast<int>(std::floor(point[2] / m_voxel_size))
-        };
-
-        return m_occupied_voxels.insert(key).second;
-    }
-
-    PointManager::PointManager(const VoxelGrid &unique_point_grid, const size_t data_size_estimate)
-        : m_unique_point_grid(unique_point_grid)
-    {
-        if (data_size_estimate > 0)
-        {
-            m_flattened_points.reserve(data_size_estimate);
-        }
-    }
-
-    void PointManager::addPoint(const std::array<float, 3> &point)
-    {
-        if (m_unique_point_grid.isPointInUniqueVoxel(point))
-        {
-            m_flattened_points.insert(m_flattened_points.end(), point.begin(), point.end());
-        }
-    }
-
-    Eigen::MatrixX3d PointManager::getMatrix()
-    {
-        return Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, 3, Eigen::RowMajor>>(
-            m_flattened_points.data(),
-            static_cast<Eigen::Index>(m_flattened_points.size()) / 3,
-            3
-        );
-    }
-
-    // ---------------------------------------------------------------------------------------------
     // Initial Parameter Estimation
     // ---------------------------------------------------------------------------------------------
 
