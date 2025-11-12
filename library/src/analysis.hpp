f358d5839e1b98dd7772e639fbe7e4e52a85fb87
Alex Farrell <ralexfarrell@outlook.com>
2025-11-12 11:58:34 -0500

    Removed unnecessary sections from analysis.hpp

--- library/src/analysis.hpp
+++ library/src/analysis.hpp
@@ -5,15 +5,7 @@
 
 namespace microstrain_mag_cal
 {
-    // ---------------------------------------------------------------------------------------------
-    // Data
-    // ---------------------------------------------------------------------------------------------
-
     double calculateSpatialCoverage(const Eigen::MatrixX3d &points, const Eigen::RowVector3d &initial_offset);
-
-    // ---------------------------------------------------------------------------------------------
-    // Fit
-    // ---------------------------------------------------------------------------------------------
 
     double calculateFitRMSE(const Eigen::MatrixX3d &points, const FitResult &fit_result, double field_strength);
 }
