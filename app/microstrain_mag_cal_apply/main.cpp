4707678fdfdd814c6a65ced5814479e13c8f8697
Alex Farrell <ralexfarrell@outlook.com>
2025-12-17 15:52:01 -0500

    Setup cli parsing for calibration filepath in apply cli

--- app/microstrain_mag_cal_apply/main.cpp
+++ app/microstrain_mag_cal_apply/main.cpp
@@ -1,6 +1,7 @@
 #include <filesystem>
 #include <iostream>
 
+#include <CLI/CLI.hpp>
 #include <nlohmann/json.hpp>
 
 #include <microstrain/connections/serial/serial_connection.hpp>
@@ -11,11 +12,10 @@
 static constexpr const char *PORT_NAME = "COM37";
 static constexpr uint32_t    BAUDRATE  = 115200;
 
-std::filesystem::path json_file = "C:/Users/AFARRELL/Downloads/ellipsoidal_fit.json";
-
-
 // TODO: Where to put this? Can move to app backend or is there some microstrain utility module or something
 //       where this could be useful?
+// TODO: Add test with same data types as soft-iron matrix and hard-iron offset to check row-major/column-major
+//       compatibility (i.e. want to make sure it's in the proper format when written to the device.
 template<typename T, typename Derived>
 std::vector<T> toArray(const Eigen::MatrixBase<Derived>& matrix)
 {
@@ -26,49 +26,76 @@ std::vector<T> toArray(const Eigen::MatrixBase<Derived>& matrix)
 }
 
 
-int main()
+int main(const int argc, char **argv)
 {
-    microstrain_mag_cal::FitResult fit_result = microstrain_mag_cal::deserializeFitResultFromFile(json_file);
+    // Required arguments
+    std::filesystem::path arg_calibration_filepath;
+
+    CLI::App app{"Command-line tool to apply a magnetometer calibration to a device."};
+    app.usage("Usage: " + std::filesystem::path(argv[0]).filename().string() + " <file> [OPTIONS]");
+
+    app.add_option("file", arg_calibration_filepath, "JSON file containing a calibration to apply.")
+        ->check(CLI::ExistingFile)
+        ->multi_option_policy(CLI::MultiOptionPolicy::Throw)
+        ->required();
+
+    CLI11_PARSE(app, argc, argv);
+
+    microstrain_mag_cal::FitResult fit_result = microstrain_mag_cal::deserializeFitResultFromFile(arg_calibration_filepath);
 
     if (fit_result.error == microstrain_mag_cal::FitResult::Error::DESERIALIZATION_COULD_NOT_OPEN_FILE)
     {
-        printf("ERROR: Could not open file to deserialize: %s", json_file.filename().string().c_str());
+        printf("ERROR: Could not open file to deserialize: %s", arg_calibration_filepath.filename().string().c_str());
 
         return 1;
     }
 
     if (fit_result.error != microstrain_mag_cal::FitResult::Error::NONE)
     {
-        std::cout << "WARNING: Applying calibration that contains error code: " << magic_enum::enum_name(fit_result.error);
+        std::cout << "ERROR: Calibration contains error: " << magic_enum::enum_name(fit_result.error) << "\n";
+
+        return 1;
     }
 
     microstrain::connections::SerialConnection connection(PORT_NAME, BAUDRATE);
 
     if (!connection.connect())
     {
-        printf("ERROR: Connection Failed.\n");
+        printf("ERROR: Failed to connect to device with\n");
+        printf("    --->     Port: %s\n", PORT_NAME);
+        printf("    ---> Baudrate: %d", BAUDRATE);
+
+        return 1;
     }
 
     mip::Interface device(&connection, mip::C::mip_timeout_from_baudrate(BAUDRATE), 2000);
 
     if (!mip::commands_3dm::writeMagHardIronOffset(device, toArray<float>(fit_result.hard_iron_offset).data()))
     {
-        printf("ERROR: Updating hard-iron offset failed.");
+        printf("ERROR: Writing hard-iron offset failed.");
 
-        if (!mip::commands_3dm::saveMagHardIronOffset(device))
-        {
-            printf("ERROR: Saving hard-iron offset failed.");
-        }
+        return 1;
+    }
+
+    if (!mip::commands_3dm::saveMagHardIronOffset(device))
+    {
+        printf("ERROR: Saving hard-iron offset failed.");
+
+        return 1;
     }
 
     if (!mip::commands_3dm::writeMagSoftIronMatrix(device, toArray<float>(fit_result.soft_iron_matrix).data()))
     {
-        printf("ERROR: Updating soft-iron matrix failed.");
+        printf("ERROR: Writing soft-iron matrix failed.");
 
-        if (!mip::commands_3dm::saveMagSoftIronMatrix(device))
-        {
-            printf("ERROR: Saving soft-iron matrix failed.");
-        }
+        return 1;
+    }
+
+    if (!mip::commands_3dm::saveMagSoftIronMatrix(device))
+    {
+        printf("ERROR: Writing soft-iron matrix failed.");
+
+        return 1;
     }
 
     return 0;
