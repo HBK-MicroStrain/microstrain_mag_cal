d224cb4dde82e3346dc29ed55b3bcb2a90049b2a
Alex Farrell <ralexfarrell@outlook.com>
2026-01-07 10:21:18 -0500

    Added test skeletons for combined correction functions

---
+++ lib/test/combined_coefficients.unit.cpp
@@ -0,0 +1,15 @@
+#include <microstrain_mag_cal_apply/combined_coefficients.hpp>
+#include <microstrain_test/microstrain_test.hpp>
+#include "clean_data.hpp"
+#include "shared_fixtures.hpp"
+
+using namespace microstrain_mag_cal;
+
+
+MICROSTRAIN_TEST_CASE("Lib_Apply", "Applying_combined_soft_iron_corrections_is_equivalent_to_applying_them_sequentially")
+{
+}
+
+MICROSTRAIN_TEST_CASE("Lib_Apply", "Applying_combined_hard_iron_offsets_is_equivalent_to_applying_them_sequentially")
+{
+}
