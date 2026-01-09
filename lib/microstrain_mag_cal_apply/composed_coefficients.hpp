ac0de431fb7ad1fca709234c06dd2ffe80507c87
Alex Farrell <ralexfarrell@outlook.com>
2026-01-09 16:20:07 -0500

    Added functions in apply lib for converting to Eigen format from data arrays

--- lib/microstrain_mag_cal_apply/composed_coefficients.hpp
+++ lib/microstrain_mag_cal_apply/composed_coefficients.hpp
@@ -6,4 +6,18 @@
 namespace microstrain_mag_cal
 {
     FitResult composeCorrections(const FitResult &old_fit, const FitResult &new_fit);
+
+    template<typename T>
+    Eigen::Matrix3d toSoftIronMatrix(const T* row_major_data)
+    {
+        return Eigen::Map<const Eigen::Matrix<T, 3, 3, Eigen::RowMajor>>(row_major_data)
+            .template cast<double>();
+    }
+
+    template<typename T>
+    Eigen::Vector3d toHardIronOffset(const T* data)
+    {
+        return Eigen::Map<const Eigen::Vector<T, 3>>(data)
+            .template cast<double>();
+    }
 };
